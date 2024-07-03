3e133e58264e63d244be306920f9a4e72a43b58e
JuanCarlosLopezHortelano <juancarloslhhellin@gmail.com>
2024-07-03 12:41:10 +0200

    MVT

--- coffee_shop/products/models.py
+++ coffee_shop/products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.TextField(max_length=200, verbose_name="nombre")
+    descripcion = models.TextField(max_length=200, verbose_name="descripcion")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="precio")
+    available = models.BooleanField(default=True)
+    photo = models.ImageField(upload_to="logos", null=True, blank=True, verbose_name="disponible")
+    
+    def __str__(self):
+        return str(self.name)
+    
