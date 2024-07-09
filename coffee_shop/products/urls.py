334da91fe54b4f49b4fdbd8d4ed79e0ff90d8f82
JuanCarlosLopezHortelano <juancarloslhhellin@gmail.com>
2024-07-09 17:10:00 +0200

    ListaProductos - nuevo template - agregarUrl - nuevaVisa - baseProyecto

--- coffee_shop/products/urls.py
+++ coffee_shop/products/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import ProductFormView
+from .views import ProductFormView, ProductListView
 
 urlpatterns = [
-    path('agregar/', ProductFormView.as_view(), name="add_product"),
+    path('', ProductListView.as_view(), name="list_product"),
+    path('agregar/', ProductFormView.as_view(), name="add_product")
+    
+
 ]
